7dad0aa6e729884c9e7f006436ea28872f0c9813
Abir Shaikh <shaikhabir044@gmail.com>
2025-12-30 21:43:36 +0530

    correction

--- src/LinkedList/LinkedlistRevision/SinglyLinkedListMake.java
+++ src/LinkedList/LinkedlistRevision/SinglyLinkedListMake.java
@@ -1,17 +1,17 @@
 package LinkedList.LinkedlistRevision;
 
 public class SinglyLinkedListMake {
-    static class Node {
+    public static class Node {
         int data;
         Node next;
 
-        public Node(int data) {
+        Node(int data) {
             this.data = data;
-            this.next = next;
+            this.next = null;
         }
     };
 
-    static void main(String[] args) {
+    public static void main(String[] args) {
         int[] arr = {1 , 3 , 6 , 8};
         Node node = new Node(arr[3]);
         System.out.println("Found : " + node.data);
