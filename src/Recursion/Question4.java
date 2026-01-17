0c94554b86e58a000fc7959232240f86bd4f9b52
Abir Shaikh <shaikhabir044@gmail.com>
2026-01-17 10:38:25 +0530

    print 1 to n without using loop

---
+++ src/Recursion/Question4.java
@@ -0,0 +1,20 @@
+package Recursion;
+
+public class Question4 {
+
+    public void printNumbers(int n) {
+        if (n == 0) {
+            return;
+        }
+        printNumbers(n-1);
+        System.out.print(n + " ");
+    }
+
+    public static void main(String[] args) {
+        Question4 obj = new Question4();
+
+        int n = 20;
+        System.out.print("Numbers -> ");
+        obj.printNumbers(n);
+    }
+}
