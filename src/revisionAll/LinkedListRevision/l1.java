8d1f16d2fe84fef4601ab433cf5c1e0a554805ee
Abir Shaikh <shaikhabir044@gmail.com>
2026-03-01 14:04:50 +0530

    revision of sll traversal

--- src/revisionAll/LinkedListRevision/l1.java
+++ src/revisionAll/LinkedListRevision/l1.java
@@ -1,4 +1,32 @@
 package revisionAll.LinkedListRevision;
 
 public class l1 {
+    public static class Node{
+        int data;
+        Node next;
+
+        Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static void traverse(Node head){
+        Node temp = head;
+        while(temp != null){
+            System.out.print(temp.data +"->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+
+
+    static void main(String[] args) {
+        Node head = new Node(4);
+        head.next = new Node(5);
+        head.next.next = new Node(6);
+        head.next.next.next = new Node(10);
+        traverse(head);
+    }
 }
