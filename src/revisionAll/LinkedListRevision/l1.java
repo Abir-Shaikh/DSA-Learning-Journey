791b8b1a86a21fad06b86ba7d2694e295e6cede6
Abir Shaikh <shaikhabir044@gmail.com>
2026-03-01 14:15:11 +0530

    revision of sll deletion of head

--- src/revisionAll/LinkedListRevision/l1.java
+++ src/revisionAll/LinkedListRevision/l1.java
@@ -20,13 +20,28 @@ public class l1 {
         System.out.println("null");
     }
 
-
+    //delete head
+    public static Node DeleteHead(Node head){
+        if (head == null) {
+            return head;
+        }
+        head = head.next;
+        return head;
+    }
 
     static void main(String[] args) {
         Node head = new Node(4);
         head.next = new Node(5);
         head.next.next = new Node(6);
         head.next.next.next = new Node(10);
+        head.next.next.next.next = new Node(12);
+
+        System.out.println("Before Deleting head : ");
+        traverse(head);
+
+        head = DeleteHead(head);
+
+        System.out.println("After deleting head : ");
         traverse(head);
     }
 }
