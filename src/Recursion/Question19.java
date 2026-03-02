cdfc696ccc3e9d99313cc8e725ddc77f6c9df3e7
Abir Shaikh <shaikhabir044@gmail.com>
2026-03-02 18:30:06 +0530

    permutation of array

---
+++ src/Recursion/Question19.java
@@ -0,0 +1,29 @@
+package recursion;
+
+import java.util.ArrayList;
+
+public class Question19 {
+
+    public static void permutation(int[] nums , ArrayList<Integer> dabba , boolean[] track){
+        if(dabba.size() == nums.length){
+            System.out.println(dabba);
+            return;
+        }
+        for (int i = 0; i < nums.length ; i++) {
+            if (!track[i]) {
+                dabba.add(nums[i]);
+                track[i] = true;
+                permutation(nums, dabba, track);
+                dabba.remove(dabba.size() - 1);
+                track[i] = false;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,3};
+        boolean[] track = new boolean[3];
+        ArrayList<Integer> dabba = new ArrayList<>();
+        permutation(nums , dabba, track);
+    }
+}
