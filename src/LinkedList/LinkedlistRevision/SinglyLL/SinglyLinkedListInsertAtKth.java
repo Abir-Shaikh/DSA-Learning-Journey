d7fccafde02bc00190a66cb39eb6bde23e92fad9
Abir-Shaikh <shaikhabir044@gmail.com>
2026-04-18 14:04:41 +0530

    insertioj of kth element revised

--- src/LinkedList/LinkedlistRevision/SinglyLL/SinglyLinkedListInsertAtKth.java
+++ src/LinkedList/LinkedlistRevision/SinglyLL/SinglyLinkedListInsertAtKth.java
@@ -44,7 +44,7 @@ public class SinglyLinkedListInsertAtKth {
         return head;
     }
 
-    static void main(String[] args) {
+    public static void main(String[] args) {
         Node head = new Node(2);
         head.next = new Node(3);
         head.next.next = new Node(6);
--- src/revisionAll/LinkedListRevision/singlyLinkedList.java
+++ src/revisionAll/LinkedListRevision/singlyLinkedList.java
@@ -41,6 +41,31 @@ public class singlyLinkedList {
         return head;
     }
 
+    //insert at kth elememnt
+    public static Node insertAtKth(Node head , int val , int k){
+        if (head == null) return null;
+
+        //for 1st element
+        if (k == 1) {
+            Node node = new Node(val);
+            node.next = head;
+            return node;
+        }
+        int counter = 0;
+        Node temp = head;
+        while (temp!= null){
+            counter++;
+            if (counter == k-1) {
+                Node x = new Node(val);
+                x.next = temp.next;
+                temp.next = x;
+                return head;
+            }
+        temp = temp.next;
+        }
+        return head;
+    }
+
     public static void main(String[] args) {
         Node head = new Node(2);
         head.next = new Node(3);
@@ -50,9 +75,13 @@ public class singlyLinkedList {
         System.out.println("Before inserting head: ");
         traverse(head);
 
-        head = insertAtTail(head , 5);
+        head = insertAtKth(head , 5 , 5);
 
         System.out.println("After inserting head: ");
         traverse(head);
+//        Node head = null;
+//        Node result = insertAtKth(head, 5, 1);
+//        System.out.println("Inserting into null list: ");
+//        traverse(result);
     }
 }
