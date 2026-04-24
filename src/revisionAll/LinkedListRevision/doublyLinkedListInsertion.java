248bfb06af06ae85171e16deb04c3ffc1dfff112
Abir-Shaikh <shaikhabir044@gmail.com>
2026-04-24 08:24:28 +0530

    arr to dll

--- src/revisionAll/LinkedListRevision/doublyLinkedListInsertion.java
+++ src/revisionAll/LinkedListRevision/doublyLinkedListInsertion.java
@@ -1,5 +1,6 @@
 package revisionAll.LinkedListRevision;
 
+
 public class doublyLinkedListInsertion {
     public static class Node{
         int data;
@@ -13,7 +14,38 @@ public class doublyLinkedListInsertion {
         }
     }
 
-    public static void main(String[] args) {
+    private static void traverse(Node head){
+        Node temp = head;
+        while(temp!= null){
+            System.out.print(temp.data + " <-> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
 
+    private static Node convertArrtoDLL(int[] arr){
+        Node head = new Node(arr[0]);
+        Node prev = head;
+
+        for (int i = 1; i < arr.length; i++) {
+            Node temp = new Node(arr[i]);
+            prev.next = temp;
+            prev = temp;
+        }
+        return head;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10 , 20 , 30 , 40 , 50};
+        Node head = convertArrtoDLL(arr);
+
+
+        System.out.println("Before : ");
+        traverse(head);
+
+        head = convertArrtoDLL(arr);
+
+        System.out.println("After : ");
+        traverse(head);
     }
 }
